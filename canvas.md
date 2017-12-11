ed46def5e896f810a6f6279259492c243a5d83b1
Yang Yusheng <nearsmle@163.com>
2017-12-11 09:05:22 +0800

    update: line property

---
+++ canvas.md
@@ -0,0 +1,6 @@
+# canvas note
+
+## line
+
+* lineJoin属性
+  * miterLimit规定了一个自动填充连接点的极限值。如果超过了这个值，会导致lineJoin属性失效，会从 miter 变成 bevel。可以看出来，这个值和线宽以及夹角有关
--- src/containers/line.vue
+++ src/containers/line.vue
@@ -8,25 +8,53 @@ export default {
   name: 'line',
   mounted () {
     this.lineCap()
+    this.lineJoin()
   },
   methods: {
-    lineCap () {
+    initCanvas () {
       const canvas = document.querySelector('canvas')
       const scrollWidth = document.documentElement.scrollWidth || document.body.scrollWidth
       const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
       canvas.width = scrollWidth
       canvas.height = scrollHeight
       const cxt = canvas.getContext('2d')
+      return cxt
+    },
 
-      cxt.lineWidth = 10
-      cxt.strokeStyle = '1baaaa'
-
+    // 端点样式
+    lineCap () {
+      const cxt = this.initCanvas()
+      this.drawLine(cxt, 100, 100, 100, 150, '', 2, 'red')
+      this.drawLine(cxt, 400, 100, 400, 150, '', 2, 'red')
+      this.drawLine(cxt, 100, 100, 400, 100, 'butt', 10, 'green')
+      this.drawLine(cxt, 100, 120, 400, 120, 'round', 10)
+      this.drawLine(cxt, 100, 140, 400, 140, 'square', 10)
+    },
+    drawLine (cxt, x, y, x2, y2, type, w, color) {
       cxt.beginPath()
-      cxt.moveTo(100, 100)
-      cxt.lineTo(400, 100)
-      // cxt.lineCap = 'butt'
-      // cxt.lineCap = 'round'
-      cxt.lineCap = 'square'
+      cxt.strokeStyle = color
+      cxt.lineWidth = w
+      cxt.moveTo(x, y)
+      cxt.lineTo(x2, y2)
+      cxt.lineCap = type
+      cxt.stroke()
+    },
+
+    // 折线样式
+    lineJoin () {
+      const cxt = this.initCanvas()
+      this.drawLineJoin(cxt, 10, 200, 110, 250, 10, 300, 10, 'green', 'miter')
+      this.drawLineJoin(cxt, 50, 200, 150, 250, 50, 300, 10, 'green', 'bevel')
+      this.drawLineJoin(cxt, 90, 200, 190, 250, 90, 300, 10, 'green', 'round')
+    },
+    drawLineJoin (cxt, x, y, x2, y2, x3, y3, w, color, type) {
+      cxt.beginPath()
+      cxt.strokeStyle = color
+      cxt.lineWidth = w
+      cxt.lineJoin = type
+      cxt.moveTo(x, y)
+      cxt.lineTo(x2, y2)
+      cxt.lineTo(x3, y3)
       cxt.stroke()
     }
   }
