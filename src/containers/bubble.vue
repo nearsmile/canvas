a63d9da438c31afec8fc7f3517f5a6f7fbf41b6e
Yang Yusheng <nearsmle@163.com>
2017-12-20 09:18:24 +0800

    update: bubble game

---
+++ src/containers/bubble.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="bubble">
+  <canvas></canvas>
+  <!-- <canvas @mousemove="handle($event)"></canvas> -->
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'bubble',
+    data () {
+      return {
+        canvas: null,
+        cxt: null,
+        balls: null
+      }
+    },
+    methods: {
+      initCanvas () {
+        const canvas = document.querySelector('canvas')
+        const width = document.documentElement.scrollWidth || document.body.scrollWidth
+        const height = document.documentElement.scrollHeight || document.body.scrollHeight
+        canvas.width = width
+        canvas.height = height
+        const cxt = canvas.getContext('2d')
+        return {
+          canvas,
+          cxt,
+          width,
+          height
+        }
+      },
+      bubbles (canvas, cxt, width, height) {
+        let balls = []
+
+        for (let i = 0; i < 50; i++) {
+          let _ball = {
+            x: Math.random() * width,
+            h: Math.random() * height,
+            r: Math.random() * 50 + 20
+          }
+          balls[i] = _ball
+        }
+        return balls
+      },
+      /* handle (event) {
+        console.log(this.canvas)
+        this.detect(event, this.canvas, this.cxt, this.balls)
+      }, */
+      draw (cxt, balls) {
+        for (let i = 0; i < balls.length; i++) {
+          cxt.beginPath()
+          cxt.arc(balls[i].x, balls[i].y, balls[i].r, 0, Math.PI * 2)
+          cxt.globalAlpha = 0.5
+
+          let R = Math.floor(Math.random() * 255)
+          let G = Math.floor(Math.random() * 255)
+          let B = Math.floor(Math.random() * 255)
+
+          cxt.fillStyle = `rgb(${R}, ${G}, ${B})`
+          cxt.fill()
+        }
+      },
+      detect (event, canvas, cxt, balls) {
+        // console.log()
+        let x = event.clientX - canvas.getBoundingClientRect().left
+        let y = event.clientY - canvas.getBoundingClientRect().top
+
+        for (let i = 0; i < balls.length; i++) {
+          cxt.beginPath()
+          cxt.arc(balls[i].x, balls[i].y, balls[i].r, 0, Math.PI * 2)
+
+          if (cxt.isPointInPath(x, y)) {
+            cxt.fillStyle = `rgba(255, 255, 255, 0.1)`
+            cxt.fill()
+          }
+        }
+      }
+    },
+    mounted () {
+      const { canvas, cxt, width, height } = this.initCanvas()
+      const balls = this.bubbles(canvas, cxt, width, height)
+      this.canvas = canvas
+      this.cxt = cxt
+      console.log('balls', balls, canvas, cxt, width, height)
+      this.draw(cxt, balls)
+    }
+  }
+</script>
---
+++ src/containers/clear.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="clear">
+    <canvas></canvas>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'clear',
+    mounted () {
+      const cxt = this.initCanvas()
+      this.clear(cxt)
+    },
+    methods: {
+      initCanvas () {
+        const canvas = document.querySelector('canvas')
+        const scrollWidth = document.documentElement.scrollWidth || document.body.scrollWidth
+        const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+        canvas.width = scrollWidth
+        canvas.height = scrollHeight
+        const cxt = canvas.getContext('2d')
+        return cxt
+      },
+      clear (cxt) {
+        cxt.fillStyle = 'red'
+        cxt.fillRect(0, 0, 416, 736)
+
+        cxt.clearRect(0, 0, 416, 736)
+      }
+    }
+  }
+</script>
